d5b4c2e2c6487171b0ee9c27b52b886574e34bc3
APiRAK <apirak.aeam@gmail.com>
2026-08-28 00:30:23 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import styles from "@/styles/Home.module.css"
-import Image from "next/image"
-import Link from "next/link"
 import Head from "next/head"
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch("https://dummyjson.com/products?limit=12&skip=10&select=title,price")
+  const data = await res.json()
+  console.log(data)
+  return{
+    props:{product:data.products}
+  }
+}
+export default function Index() {
   return (
-    <>
-      <Head>
-          <title>หน้าแรก | KongRuksiam</title>
-          <meta name="keywords" content="kongruksiam,ร้านค้า,ขายเสื้อผ้า"/>
-      </Head>
-      <div className={styles.container}>
-          <h1 className={styles.title}>หน้าแรกของเว็บไซต์</h1>
-          <Image src="/shopping.svg" width={400} height={400} alt="logo"/>
-          <p>ยินดีต้อนรับเข้าสู่ร้านค้าเด็กชายก้อง</p>
-          <Link href="/products" className={styles.btn}>ดูสินค้าทั้งหมด</Link>
-      </div>
-    </>
+   <>
+   <Head>
+      <title>สินค้าทั้งหมด | Apirak</title>
+      <meta name="keyword" content="Apirak,Shop,Sale"/>
+   </Head>
+   <h1>สินค้าทั้งหมด</h1>
+   </>
   )
 }
