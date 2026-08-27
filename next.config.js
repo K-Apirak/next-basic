3198134205f20c31877818f6acc1582a9130e8d3
APiRAK <apirak.aeam@gmail.com>
2026-08-28 01:13:32 +0700

    test commit

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.dummyjson.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+      },
     ],
   },
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
-import styles from "@/styles/Home.module.css"
 import Head from "next/head"
 export async function getStaticProps() {
-  const res = await fetch("https://dummyjson.com/products?limit=12&skip=10&select=title,price")
+  const res = await fetch("https://dummyjson.com/products?limit=12")
   const data = await res.json()
   console.log(data)
   return{
     props:{product:data.products}
-    props:{product:data.products}
-    props:{product:data.products}
-    props:{product:data.products}
-    props:{product:data.products}
   }
 }
-export default function Index() {
+export default function Index({products}) {
   return (
    <>
-   <Head>
-      <title>สินค้าทั้งหมด | Apirak</title>
-      <meta name="keyword" content="Apirak,Shop,Sale"/>
-   </Head>
-   <h1>สินค้าทั้งหมด</h1>
+      <Head>
+        <title>สินค้าทั้งหมด | Apirak</title>
+        <meta name="keywords" content="Apirak,Shop,Sale"/>
+      </Head>
+      <h1>สินค้าทั้งหมด</h1>
    </>
   )
 }
